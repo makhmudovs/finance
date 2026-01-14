a19fac57b2062290b3f67484ac76e8e55d34a5fa
makhmudovs <makhmudovs50@gmail.com>
2026-01-14 16:02:00 +0800

    budgets schema

---
+++ db/schema/budget.ts
@@ -0,0 +1,35 @@
+import {
+  pgTable,
+  text,
+  numeric,
+  timestamp,
+  boolean,
+  uuid,
+} from "drizzle-orm/pg-core";
+import { categoryEnum } from "./enums";
+import { usersTable } from "./user";
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+
+export const budgetsTable = pgTable("budgets", {
+  id: uuid("id").defaultRandom().primaryKey(),
+
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => usersTable.id, { onDelete: "cascade" }),
+
+  name: text("name").notNull(),
+  category: categoryEnum("category").notNull(),
+
+  limit: numeric("limit", { precision: 12, scale: 2 }).notNull(),
+  spent: numeric("spent", { precision: 12, scale: 2 }).default("0").notNull(),
+
+  period: text("period").notNull(), // e.g. 2025-01
+  startDate: timestamp("start_date").notNull(),
+  endDate: timestamp("end_date").notNull(),
+
+  isActive: boolean("is_active").default(true).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export type BudgetType = InferSelectModel<typeof budgetsTable>;
+export type BudgetTableInsertType = InferInsertModel<typeof budgetsTable>;
